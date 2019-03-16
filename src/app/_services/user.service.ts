da5b7f313b6dd0e22e34df28dfd1686d3ac14385
Ícaro Falcão <vbsfalcao@gmail.com>
2019-03-16 10:08:39 -0300

    Modify updateUser to persist changes in member edit component

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -20,4 +20,7 @@ export class UserService {
     return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 
+  updateUser(id: number, user: User) {
+    return this.http.put<User>(this.baseUrl + 'users/' + id, user);
+  }
 }
